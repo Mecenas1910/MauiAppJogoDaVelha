6e5579452a0e27a8de3fcb7f0d0b5ad019f651b8
Mecenas1910 <arthur.frc@outlook.com>
2024-11-17 18:48:16 -0300

    MainPage.xaml.cs

--- MauiAppJogoDaVelha/MainPage.xaml.cs
+++ MauiAppJogoDaVelha/MainPage.xaml.cs
@@ -26,25 +26,52 @@
             }
 
             /* Verificando se o X ganhou na 1ª linha */
-            if(btn10.Text == "X" && btn11.Text == "X" && btn12.Text == "X") 
-            {
-                DisplayAlert("Parabéns!", "O X ganhou!", "OK");
-                Zerar();
-            }
+            // Verificação de vitória nas linhas horizontais
+if (btn10.Text == "X" && btn11.Text == "X" && btn12.Text == "X" ||
+    btn20.Text == "X" && btn21.Text == "X" && btn22.Text == "X" ||
+    btn30.Text == "X" && btn31.Text == "X" && btn32.Text == "X")
+{
+    DisplayAlert("Parabéns!", "O X ganhou!", "OK");
+    Zerar();
+}
+// Verificação de vitória nas colunas
+else if (btn10.Text == "X" && btn20.Text == "X" && btn30.Text == "X" ||
+         btn11.Text == "X" && btn21.Text == "X" && btn31.Text == "X" ||
+         btn12.Text == "X" && btn22.Text == "X" && btn32.Text == "X")
+{
+    DisplayAlert("Parabéns!", "O X ganhou!", "OK");
+    Zerar();
+}
+// Verificação de vitória nas diagonais
+else if (btn10.Text == "X" && btn21.Text == "X" && btn32.Text == "X" ||
+         btn12.Text == "X" && btn21.Text == "X" && btn30.Text == "X")
+{
+    DisplayAlert("Parabéns!", "O X ganhou!", "OK");
+    Zerar();
+}
+// Repetição para "O"
+else if (btn10.Text == "O" && btn11.Text == "O" && btn12.Text == "O" ||
+         btn20.Text == "O" && btn21.Text == "O" && btn22.Text == "O" ||
+         btn30.Text == "O" && btn31.Text == "O" && btn32.Text == "O")
+{
+    DisplayAlert("Parabéns!", "O O ganhou!", "OK");
+    Zerar();
+}
+else if (btn10.Text == "O" && btn20.Text == "O" && btn30.Text == "O" ||
+         btn11.Text == "O" && btn21.Text == "O" && btn31.Text == "O" ||
+         btn12.Text == "O" && btn22.Text == "O" && btn32.Text == "O")
+{
+    DisplayAlert("Parabéns!", "O O ganhou!", "OK");
+    Zerar();
+}
+else if (btn10.Text == "O" && btn21.Text == "O" && btn32.Text == "O" ||
+         btn12.Text == "O" && btn21.Text == "O" && btn30.Text == "O")
+{
+    DisplayAlert("Parabéns!", "O O ganhou!", "OK");
+    Zerar();
+}
 
            
-        } // Fecha método
-
-        void Zerar()
-        {
-            btn10.Text = "";
-            btn11.Text = "";
-            btn12.Text = "";
-
-            btn10.IsEnabled = true;
-            btn11.IsEnabled = true;
-            btn12.IsEnabled = true;
-        }
-
+        } // Fecha método 
     } // Fecha Classe
 } // Fecha Namespace
